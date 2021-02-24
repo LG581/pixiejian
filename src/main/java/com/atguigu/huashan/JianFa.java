9ed356be5f3e4c578a4c15abc8023cad8143ca56
LG581 <1403880804@qq.com>
2021-02-24 21:03:25 +0800

    辟邪剑谱

---
+++ src/main/java/com/atguigu/huashan/JianFa.java
@@ -0,0 +1,9 @@
+package com.atguigu.huashan;
+
+public class JianFa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑谱");
+        System.out.println("辟邪剑谱");
+        System.out.println("辟邪剑谱");
+    }
+}
